1ec3bdf077709955380854585510be590210b7b5
claybro2005 <claybro2005@gmail.com>
2017-12-18 17:46:38 -0500

    cleared out errors and updated parser

--- motor_w_encoder/motor_w_encoder.ino
+++ motor_w_encoder/motor_w_encoder.ino
@@ -1,8 +1,8 @@
 /*   12/16/2017 read values and write to motor control 
 
 Ver 1, Clayton Broman 
-Program looks for a \n or \r at the end of all commands, case is ignored throughout, function,axis,
-
+Program looks for a \n or \r at the end of all commands, lower case must be used throughout, function,axis,
+format is function,axis,type (rel, abs),value 
 common commands: 
 IDN?, returns, Arduino, the version of this firmware and 
 help, returns a list of commands
@@ -39,7 +39,10 @@ int in2 = 5;
 int ind = 0;
 char strVal[10];
 boolean newData= false;
-
+String func = "", type ="", ;
+bool func_f, axis_f;
+int axis;
+int8_t tmpdata;
 
 void setup() {
 
@@ -52,7 +55,9 @@ void setup() {
   /* setup the motor pins as outputs*/
   pinMode(in1,OUTPUT);
   pinMode(in2,OUTPUT);
- 
+  //pinMode(in3,OUTPUT);
+  //pinMode(in4,OUTPUT);
+
   /* Startup serial */
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
@@ -62,8 +67,9 @@ void setup() {
 void loop() {
 
    //read serial 
-   recvOneChar();
-
+   recv();
+   //reset the variables
+   func_f = false;axis_f=false;
    static uint8_t counter = 0;      //this variable will be changed by encoder input
    int8_t tmpdata;
    /**/
@@ -72,66 +78,73 @@ void loop() {
       counter += tmpdata;
    }
    
-   showNewData();
+   go();
 }
 
-void recvOneChar() {
+void recv() {
  while(Serial.available()){  
      delay(3);
      char ch = Serial.read();
      strVal[ind++] = ch;
      //*** check for delimiter or end of line
      if(ch == ',' || ch =='\n' || ch=='\r'){
-         if(func_f == false){
-          //if function hasnt been recieved
-            if(strVal.equalsIgnoreCase(mv){
-              ///move
-            }
-            else if(strVal.equalsIgnoreCase('idn?'){
-               Serial.print("Arduino, FA Positioner, Ver1");
-               //break out of this
-            }
-            else if(strVal.equalsIgnoreCase('help'){
-               Serial.print("Help");
-               //break out of this
-            }
-            else if(strVal.equalsIgnoreCase('rst'){
-               // set rst to true
-               //
-            }
-            else if(strVal.equalsIgnoreCase('pos'){
-               //set pos to true
-            }
-            else{
-              //err(unknown function", strVal )
-            }
-            func = true;
-            strVal = "";
-         }
-         if(func_f == true && axis_f == false){
-            if(strVal.equalsIgnoreCase('pitch'){
-               axis = 1; // about the y-axis
-               //if pos or reset break and go do that
-            }
-            else if(strVal.equalsIgnoreCase('yaw'){
-               axis = 2; //about the z-axis
-               //if pos or reset break and go do that
-            }
-            else if(strVal.equalsIgnoreCase('roll'){
-               axis = 0; //about the x-axis
-               //if pos or reset break and go do that
-            }
-            axis = true;
-            strVal="";
-         }
-         if(func == 'mv'){
-            
-         }
+           if(func_f == true && axis_f == false){
+               if(strVal == 'pitch'){
+                   axis = 1; // about the y-axis
+                   //if pos or reset break and go do that
+                }
+           else if(strVal == 'yaw'){
+                  axis = 2; //about the z-axis
+                  //if pos or reset break and go do that
+                  }
+           else if(strVal == 'roll'){
+                 axis = 0; //about the x-axis
+                 //if pos or reset break and go do that
+                 }
+           axis_f = true;
+           clr_var(strVal);
+           }
+           else if(func_f == false){
+              //if function hasnt been recieved
+              if(strVal == 'mv' ){
+                   ///move
+                   func_f = true; 
+                   func = "move";
+                   }
+              else if(strVal == 'idn?'){
+                   Serial.print("Arduino, FA Positioner, Ver1");
+                   //break out of this
+                   func_f = true;
+                   func = 'idn';
+              }
+              else if(strVal == 'help'){
+                   Serial.print("Help");
+                   //break out of this
+                   func_f = true;
+              }
+              else if(strVal == 'rst'){
+                  // set rst to true
+                  //
+              }
+              else if(strVal == "pos"){
+                  //set pos to true
+              }
+              else{
+                  //err(unknown function", strVal )
+                  Serial.println("Sorry, come again?");
+              }
+              func_f = true;
+              clr_var(strVal);
+          }
+          if(func == "move" && func_f ==true && type== false && axis== false){
+             
+              Serial.println('ok now what?');
+          }
      
-     }
-     
-
-     //****  check for variables***// 
+       }
+   }
+ /* 
+     //  check for variables// 
 
      if (isDigit(strVal)){
          = int(ch);
@@ -140,16 +153,17 @@ void recvOneChar() {
         Serial.println(ch);   
         }
 
-     //******** return an error**//
-     else(){
+     //******** return an error//
+     else{
       Serial.print("Error unknown command ", strVal);   
      }
      newData = true;
    }
+   */
 }
 
-void showNewData() {
- if (newData == 1) {
+void go() {
+ if (newData == true) {
    if(dir=='1'){
       digitalWrite(in1,LOW); // stop just in case it was going the other way it wont lurch
       digitalWrite(in2,LOW);
@@ -174,10 +188,13 @@ void showNewData() {
  }
 }
 
+void clr_var(String val){
+   for( int i = 0; i < sizeof(val);  ++i )
+      val[i] = '\0';
+}
  
 /* returns change in encoder state (-1,0,1) */
-int8_t read_encoder()
-{
+int8_t read_encoder(){
    static int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0};
    static uint8_t old_AB = 0;
    /**/
